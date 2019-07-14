9dbd479cd63d07c4fcecafc197241e403a610a30
Manuel Schmidt <mschmidt@indra.es>
2019-07-14 18:22:41 +0200

    Add ntp to have system time up to date

--- provision.sh
+++ provision.sh
@@ -39,7 +39,9 @@ EOF
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common ntp
+sudo unlink /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Europe/Madrid /etc/localtime
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
