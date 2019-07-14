2ff4c57b1314ac4220cb55735f95212b1fdda781
Manuel Schmidt <mschmidt@indra.es>
2019-07-14 18:21:53 +0200

    Add elasticsearch red status as a status that indicates that elasticsearch is ready, otherwise kibana won't start ever if index is hopelessly corrupt

--- elk/elasticsearch-healthcheck.sh
+++ elk/elasticsearch-healthcheck.sh
@@ -5,7 +5,7 @@ host=$1
 
 if health="$(curl -fsSL "http://$host:9200/_cat/health?h=status")"; then
         health="$(echo "$health" | sed -r 's/^[[:space:]]+|[[:space:]]+$//g')" # trim whitespace (otherwise we'll have "green ")
-        if [ "$health" = 'green' -o "$health" = "yellow" ]; then
+        if [ "$health" = 'green' -o "$health" = "yellow" -o "$health" = "red" ]; then
                 exit 0
         fi
         echo >&2 "unexpected health status: $health"
